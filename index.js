14a28d8a301be90289c2ef40761f643f5ff514b6
andrea-agosta-salt <andrea.agosta@appliedtechnology.se>
2022-06-24 12:36:00 +0200

    Add function to check day/s left/after to start course date

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 const prompt = require('prompt-sync')();
 
-function greet(namePassedIn) {
-  return 'Welcome to SALT, ' + namePassedIn;
+function greet(namePassedIn,datePassedIn) {
+  const days = checkDay(datePassedIn);
+  return { 
+    welcome:'Welcome to SALT, ' + namePassedIn,
+    days:days
+  };
+}
+
+// Calculate how many days left/after the course
+function checkDay(date) {
+  const today = new Date();
+  const course = new Date(date);
+  let difference = Math.ceil((course - today) / (1000 * 3600 * 24));
+  if ( difference === 0){
+    return 'The course starts Today'
+  } else if(difference > 0){
+    return 'Today it is '+ difference +' days left until the course starts';
+  } else {
+    return 'Today it is '+ Math.abs(difference) +' days left after the course starts';
+  };
 }
 
 // Main program
 const name = prompt('What is your name? ');
-const greeting = greet(name);
-console.log(greeting);
+const dateStartCourse = prompt('When does that course start? ');
+const greeting = greet(name, dateStartCourse);
+console.log(greeting.welcome);
+console.log(greeting.days);
 
 module.exports.greet = greet;
